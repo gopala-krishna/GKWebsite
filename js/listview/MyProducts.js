80343d62ea0413a7ee73e76aa2c459557aefd67c
Gopala Krishna <gopala.krishna@outlook.com>
2017-04-11 05:45:36 +0530

    v4

---
+++ js/listview/MyProducts.js
@@ -0,0 +1,22 @@
+[
+    {
+        "ProductID": 1,
+        "ProductName": "http://google.com",
+        "UnitPrice": 18,
+        "UnitsInStock": 39,
+        "Discontinued": false
+    },
+    {
+        "ProductID": 2,
+        "ProductName": "http://microsoft.com",
+        "UnitPrice": 19,
+        "UnitsInStock": 17,
+        "Discontinued": false
+    },
+    {
+        "ProductID": 3,
+        "ProductName": "Aniseed Syrup",
+        "UnitPrice": 10,
+        "UnitsInStock": 13,
+        "Discontinued": false
+    }]
